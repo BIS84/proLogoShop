cccb520498abdc8fa6bc6997e0c697c6ab7955bc
BIS84 <bbiiss84@mail.ru>
2023-01-27 23:24:34 +0300

    Ссылки по имени маршрута

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -28,15 +28,15 @@
     <nav class="navbar navbar-inverse navbar-fixed-top">
         <div class="container">
             <div class="navbar-header">
-                <a class="navbar-brand" href="http://internet-shop.tmweb.ru">Интернет Магазин</a>
+                <a class="navbar-brand" href="{{ route('index') }}">Интернет Магазин</a>
             </div>
             <div id="navbar" class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="http://internet-shop.tmweb.ru">Все товары</a></li>
-                    <li><a href="http://internet-shop.tmweb.ru/categories">Категории</a>
+                    <li class="active"><a href="{{ route('index') }}">Все товары</a></li>
+                    <li><a href="{{ route('categories') }}">Категории</a>
                     </li>
-                    <li><a href="http://internet-shop.tmweb.ru/basket">В корзину</a></li>
-                    <li><a href="http://internet-shop.tmweb.ru/reset">Сбросить проект в начальное
+                    <li><a href="{{ route('basket') }}">В корзину</a></li>
+                    <li><a href="{{ route('index') }}"">Сбросить проект в начальное
                             состояние</a></li>
                     <li><a href="http://internet-shop.tmweb.ru/locale/en">en</a></li>
 
