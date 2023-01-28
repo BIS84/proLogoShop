9e6aa54e5b5f8d0f038a7b0ef3c2d1cb7f5824b2
BIS84 <bbiiss84@mail.ru>
2023-01-28 07:43:56 +0300

    Страница заказов

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\BasketController;
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 /*
@@ -30,6 +30,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/order', [OrderController::class, 'index'])->name('home');
 });
 
 require __DIR__.'/auth.php';
@@ -47,8 +48,6 @@ Route::get('/mobiles/iphone_x_64', function () {
 });
 
 Route::get('/', [MainController::class, 'index'])->name('index');
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::get('/basket', [BasketController::class, 'basket'])->name('basket');
 
