1625db90aa2dbe1225cf8ca051010684eed3fdcf
BIS84 <bbiiss84@mail.ru>
2023-01-28 10:26:58 +0300

    Добавление, редактирование и удаление товара

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\MainController;
 use App\Http\Controllers\BasketController;
 use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 /*
@@ -37,6 +38,7 @@ Route::middleware('auth')->group(function () {
 			Route::get('/orders', [OrderController::class, 'index'])->name('home');
         });
 		Route::resource('categories', CategoryController::class);
+        Route::resource('products', ProductController::class);
 	});
 });
 
