41c6d4892c988889a5df892ffc916ed0ee5f92ab
BIS84 <bbiiss84@mail.ru>
2023-01-28 06:26:28 +0300

    Добавление и удаление товаров в корзине. Подсчет общей стоимости

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -31,8 +31,46 @@ class BasketController extends Controller
         } else {
             $order = Order::find($orderId);
         }
-        $order->products()->attach($productId);
 
-        return view('basket', ['order' => $order]);
+        if($order->products->contains($productId)) { // Проверяем есть ли такой товар в корзине
+            $pivotRow = $order->products()
+                        ->where('product_id', $productId)
+                        ->first()
+                        ->pivot; // Получаем доступ к полям таблицы (для данного товара)
+            $pivotRow->count++; // Увеличиваем счетчик
+            $pivotRow->update(); // Обновляем поля
+        } else {
+            $order->products()->attach($productId); // Если товара еще не было в корзине, добавляем
+        }
+
+        return to_route('basket');
     }
+
+    public function basketRemove($productId)
+	{
+		$orderId = session('orderId');
+		$order = Order::find($orderId);
+
+		if(is_null($orderId)) {
+			return to_route('basket');
+		}
+
+        $order = Order::find($orderId);
+
+		if($order->products->contains($productId)) { // Проверяем есть ли такой товар в корзине
+            $pivotRow = $order->products()
+                        ->where('product_id', $productId)
+                        ->first()
+                        ->pivot; // Получаем доступ к полям таблицы (для данного товара)
+            if($pivotRow->count < 2) {
+                $order->products()->detach($productId); // Если такой товар в корзине один, удаляем его
+            } else {
+                $pivotRow->count--; // Уменьшаем счетчик
+                $pivotRow->update(); // Обновляем поля
+            }
+
+        }
+
+		return to_route('basket');
+	}
 }
